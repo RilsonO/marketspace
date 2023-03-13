490bf48c743c261a4d7ab6b60edb9f2fa9067b0a
Rilson Oliveira <rilson2.0@gmail.com>
2023-03-13 17:26:55 -0300

    Created the theme file and pass to NativeBaseProvider.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { NativeBaseProvider, Box, Center } from 'native-base';
+import { NativeBaseProvider, Box, Center, Text } from 'native-base';
+import {
+  useFonts,
+  Karla_400Regular,
+  Karla_700Bold,
+} from '@expo-google-fonts/karla';
+import { THEME } from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold });
+
   return (
-    <NativeBaseProvider>
-      <Center flex={1}>
-        <Box>Hello world</Box>
-      </Center>
+    <NativeBaseProvider theme={THEME}>
+      {fontsLoaded ? (
+        <Center flex={1}>
+          <Text color='blue-light' fontSize='xl' fontFamily='heading'>
+            Hello world
+          </Text>
+        </Center>
+      ) : null}
     </NativeBaseProvider>
   );
 }
