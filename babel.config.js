900b37903c09946319943cf78c9f731454e1b412
Rilson Oliveira <rilson2.0@gmail.com>
2023-04-18 16:50:33 -0300

    chore: Add alias for mappers folder in babel.config.js and tsconfig.json

--- babel.config.js
+++ babel.config.js
@@ -18,6 +18,7 @@ module.exports = function (api) {
             '@hooks': './src/hooks',
             '@contexts': './src/contexts',
             '@routes': './src/routes',
+            '@mappers': './src/mappers',
           },
         },
       ],
