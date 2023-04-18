4210a100a2d000defa2bb9f99b0e31c19c1a6fe2
Rilson Oliveira <rilson2.0@gmail.com>
2023-04-18 17:34:36 -0300

    feat: Create ProductMap to convert data from ProductDTO to IProduct.

---
+++ src/mappers/ProductMap.ts
@@ -0,0 +1,36 @@
+import { ProductDTO } from '@dtos/ProductDTO';
+import { api } from '@services/api';
+import { IProduct } from 'src/interfaces/IProduct';
+import uuid from 'react-native-uuid';
+import { PhotoMap } from './PhotoMap';
+import { PaymentMethodsMap } from './PaymentMethodsMap';
+
+class ProductMap {
+  static toIProduct({
+    id,
+    name,
+    description,
+    is_new,
+    price,
+    accept_trade,
+    payment_methods,
+    product_images,
+    user,
+  }: ProductDTO): IProduct {
+    return {
+      id: id ?? String(uuid.v4()),
+      name,
+      description,
+      is_new,
+      price,
+      accept_trade,
+      payment_methods: payment_methods.map((item) =>
+        PaymentMethodsMap.toIPaymentMethods(item)
+      ),
+      product_images: product_images.map((item) => PhotoMap.toIPhoto(item)),
+      user,
+    };
+  }
+}
+
+export { ProductMap };
