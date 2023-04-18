171f1c16fbe3794992109903c8bb26aebb9e914e
Rilson Oliveira <rilson2.0@gmail.com>
2023-04-18 18:09:17 -0300

    refactor: Improve PreviewAd screen by creating handleUpdate function, refactoring handlePublish and handleGoBack, and removing unnecessary imports.

--- src/screens/PreviewAd.tsx
+++ src/screens/PreviewAd.tsx
@@ -1,20 +1,129 @@
 import { Button } from '@components/Button';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { AppNavigatorRoutesProps } from '@routes/app.routes';
-import { HStack, Text, VStack, useTheme } from 'native-base';
+import { HStack, Text, VStack, useTheme, useToast } from 'native-base';
 import { ArrowLeft, Tag } from 'phosphor-react-native';
 import { api } from '@services/api';
-import { AdDetails, AdDetailsProps } from '@components/AdDetails';
+import { AdDetails } from '@components/AdDetails';
+import { useState } from 'react';
+import { AppError } from '@utils/AppError';
+import { IPhoto } from 'src/interfaces/IPhoto';
+import { IProduct } from 'src/interfaces/IProduct';
 
 export function PreviewAd() {
   const { colors, sizes } = useTheme();
-  const { navigate } = useNavigation<AppNavigatorRoutesProps>();
+  const { navigate, goBack } = useNavigation<AppNavigatorRoutesProps>();
   const route = useRoute();
+  const toast = useToast();
 
-  const params = route.params as AdDetailsProps;
+  const params = route.params as IProduct & { imagesToDelete: string[] };
+
+  const [isLoading, setIsLoading] = useState(false);
 
   function handleGoBack() {
-    navigate('createAd');
+    goBack();
+  }
+
+  async function handlePublish() {
+    try {
+      setIsLoading(true);
+
+      const { data } = await api.post('/products', {
+        name: params.name,
+        description: params.description,
+        is_new: params.is_new,
+        price: Number(params.price.toFixed(0)),
+        accept_trade: params.accept_trade,
+        payment_methods: params.payment_methods,
+      });
+
+      const formData = new FormData();
+      formData.append('product_id', data.id);
+
+      params.product_images.map((photo) => {
+        formData.append('images', photo as any);
+      });
+
+      await api.post('/products/images', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      navigate('myAdDetails', { id: data.id });
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível cadastrar o seu produto. Tente novamente mais tarde.';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function handleUpdate() {
+    try {
+      setIsLoading(true);
+      if (params.imagesToDelete.length > 0) {
+        await api.delete('/products/images', {
+          data: { productImagesIds: params.imagesToDelete },
+        });
+      }
+
+      await api.put(`/products/${params.id}`, {
+        name: params.name,
+        description: params.description,
+        is_new: params.is_new,
+        price: Number(params.price.toFixed(0)),
+        accept_trade: params.accept_trade,
+        payment_methods: params.payment_methods,
+      });
+
+      let imagesToUpload = [] as IPhoto[];
+      params.product_images.map((photo) => {
+        if (!photo.uri.match(`${api.defaults.baseURL}/images/`)) {
+          imagesToUpload = [...imagesToUpload, photo];
+        }
+      });
+
+      if (imagesToUpload.length > 0) {
+        const formData = new FormData();
+        formData.append('product_id', params.id as string);
+
+        imagesToUpload.map((photo) => {
+          if (!photo.uri.match(`${api.defaults.baseURL}/images/`)) {
+            formData.append('images', photo as any);
+          }
+        });
+
+        await api.post('/products/images', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+      }
+
+      navigate('myAdDetails', { id: params.id as string });
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível atualizar o seu produto. Tente novamente mais tarde.';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
@@ -38,6 +147,7 @@ export function PreviewAd() {
           marginRight={2}
           leftIcon={<ArrowLeft size={sizes[4]} color={colors.gray[600]} />}
           onPress={handleGoBack}
+          disabled={isLoading}
         />
         <Button
           title='Publicar'
@@ -45,6 +155,8 @@ export function PreviewAd() {
           bgColor='blue.400'
           leftIcon={<Tag size={sizes[4]} color={colors.gray[200]} />}
           marginLeft={2}
+          onPress={!!params.id ? handleUpdate : handlePublish}
+          isLoading={isLoading}
         />
       </HStack>
     </VStack>
