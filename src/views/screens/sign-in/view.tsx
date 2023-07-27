550af3db557a4b59efc6744e137fe3dd9e6936ab
RilsonO <rilson2.0@gmail.com>
2023-07-27 15:16:06 -0300

    refactor: Refactor SignIn screen.

--- src/views/screens/sign-in/view.tsx
+++ src/views/screens/sign-in/view.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Platform, TouchableOpacity } from 'react-native';
 import {
   VStack,
@@ -9,66 +8,27 @@ import {
   KeyboardAvoidingView,
   Box,
   useTheme,
-  useToast,
 } from 'native-base';
-import { useForm, Controller } from 'react-hook-form';
-import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
+import { Controller } from 'react-hook-form';
+
 import LogoSvg from '@assets/logo_with_name.svg';
-import { Input } from '@components/Input';
-import { Button } from '@components/Button';
+import { Input } from '@views/components/Input';
+import { Button } from '@views/components/Button';
 import { Eye, EyeSlash } from 'phosphor-react-native';
-import { useNavigation } from '@react-navigation/native';
-import { AuthNavigatorRoutesProps } from '@routes/auth.routes';
-import { AppError } from '@utils/AppError';
-import { useAuth } from '@hooks/useAuth';
-
-const signInSchema = yup.object({
-  email: yup.string().required('Informe o email.').email('E-mail inválido.'),
-  password: yup.string().required('Informe a senha.'),
-});
-
-type FormDataProps = yup.InferType<typeof signInSchema>;
+import { useSignInViewModel } from './view-model';
 
 export function SignIn() {
-  const navigation = useNavigation<AuthNavigatorRoutesProps>();
   const { colors, sizes } = useTheme();
   const {
-    control,
     handleSubmit,
-    formState: { errors },
-  } = useForm<FormDataProps>({
-    resolver: yupResolver(signInSchema),
-  });
-  const { signIn } = useAuth();
-  const toast = useToast();
-
-  const [passwordSecureTextEntry, setPasswordSecureTextEntry] = useState(true);
-  const [isLoading, setIsLoading] = useState(false);
-
-  async function handleSignIn({ password, email }: FormDataProps) {
-    try {
-      setIsLoading(true);
-      await signIn(email, password);
-    } catch (error) {
-      const isAppError = error instanceof AppError;
-      const title = isAppError
-        ? error.message
-        : 'Não foi possível entrar. Tente novamente mais tarde.';
-
-      setIsLoading(false);
-
-      toast.show({
-        title,
-        placement: 'top',
-        bgColor: 'red.500',
-      });
-    }
-  }
-
-  function handleNewAccount() {
-    navigation.navigate('signUp');
-  }
+    errors,
+    control,
+    passwordSecureTextEntry,
+    isLoading,
+    handleNewAccount,
+    handlePasswordSecureTextEntry,
+    handleSignIn,
+  } = useSignInViewModel();
 
   return (
     <KeyboardAvoidingView
@@ -84,7 +44,7 @@ export function SignIn() {
             <Center my={24}>
               <LogoSvg />
               <Text color='gray.500' fontSize='sm' fontFamily='light'>
-                Treine sua mente e o seu corpo
+                Seu espaço de compra e venda
               </Text>
             </Center>
 
@@ -124,11 +84,7 @@ export function SignIn() {
                     onChangeText={onChange}
                     errorMessage={errors.password?.message}
                     InputRightElement={
-                      <TouchableOpacity
-                        onPress={() =>
-                          setPasswordSecureTextEntry((prev) => !prev)
-                        }
-                      >
+                      <TouchableOpacity onPress={handlePasswordSecureTextEntry}>
                         {passwordSecureTextEntry ? (
                           <Eye
                             size={sizes[5]}
@@ -149,6 +105,7 @@ export function SignIn() {
               />
 
               <Button
+                testID='Enter-Button'
                 title='Entrar'
                 bgColor='blue.400'
                 onPress={handleSubmit(handleSignIn)}
@@ -162,6 +119,7 @@ export function SignIn() {
               Ainda não tem acesso?
             </Text>
             <Button
+              testID='Create-Button'
               disabled={isLoading}
               title='Criar uma conta'
               bgColor='gray.300'
