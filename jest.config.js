087b674916f8b8d2c50079bbbc7b46f9add3ea38
RilsonO <rilson2.0@gmail.com>
2023-07-27 14:41:04 -0300

    feat: Add 'jest.config.js' for Jest configuration.

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'jest-expo',
+  moduleNameMapper: {
+    '^@react-native-async-storage/async-storage$':
+      '<rootDir>/src/__mocks__/async-storage.ts',
+    '^@react-navigation/native$': '<rootDir>/src/__mocks__/navigation.ts',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+    'node_modules/(?!@react-native|react-native)',
+    '/node_modules/(?!native-base)/',
+  ],
+  cacheDirectory: '.jest/cache',
+};
