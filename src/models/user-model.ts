52907e21dec7fbd67089508f95b83daaae335f94
RilsonO <rilson2.0@gmail.com>
2023-07-27 15:03:56 -0300

    feat: Create user model.

--- src/models/user-model.ts
+++
@@ -1,9 +0,0 @@
-export interface UserModel {
-  id: string;
-  avatar: string;
-  name: string;
-  email: string;
-  tel: string;
-  signOut: () => Promise<void>;
-  updateProfile: () => Promise<void>;
-}
