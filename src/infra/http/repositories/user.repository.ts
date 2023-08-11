77b400f1aed12505a16154499e384a2d7ae1cc13
RilsonO <rilson2.0@gmail.com>
2023-08-11 11:01:23 -0300

    feat: Create signUp function into user repository.

--- src/infra/http/repositories/user.repository.ts
+++ src/infra/http/repositories/user.repository.ts
@@ -2,6 +2,30 @@ import { SignInRequestDTO, SignInResponseDTO } from '@dtos/sign-in.dtos';
 import { client } from '../client.http';
 import { UpdateProfileResponseDTO } from '@dtos/update-profile.dtos';
 import { UserProductResponseDTO } from '@dtos/product.dtos';
+import { SignUpRequestDTO } from '@dtos/sign-up.dtos';
+
+async function signUp({
+  name,
+  password,
+  email,
+  phone,
+  photo,
+}: SignUpRequestDTO) {
+  const formData = new FormData();
+  formData.append('name', name);
+  formData.append('email', email);
+  formData.append('tel', phone);
+  formData.append('password', password);
+  if (photo.uri) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    formData.append('avatar', photo as any);
+  }
+  await client.post<SignInResponseDTO>('/users', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+}
 
 async function signIn({ email, password }: SignInRequestDTO) {
   const { data } = await client.post<SignInResponseDTO>('/sessions', {
@@ -23,4 +47,4 @@ async function fetchProducts() {
   return data;
 }
 
-export { signIn, updateProfile, fetchProducts };
+export { signUp, signIn, updateProfile, fetchProducts };
