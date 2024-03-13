deb56f6dc566954578c899bd5bbd97036db7d7d9
RilsonO <rilson2.0@gmail.com>
2024-03-13 15:08:34 -0300

    feat: Create fetchProductById, productDeleteById and productToggleDisableById methods into product repository.

--- src/infra/http/repositories/product.repository.ts
+++ src/infra/http/repositories/product.repository.ts
@@ -1,9 +1,36 @@
 import { client } from '../client.http';
-import { ProductDTO, ProductsRequestDTO } from '@dtos/product.dtos';
+import {
+  ProductDTO,
+  ProductDetailDTO,
+  ProductRequestDTO,
+  ProductToggleDisableRequestDTO,
+  ProductsRequestDTO,
+} from '@dtos/product.dtos';
 
 async function fetchProducts({ filter }: ProductsRequestDTO) {
   const { data } = await client.get<ProductDTO[]>(`/products${filter}`);
   return data;
 }
 
-export { fetchProducts };
+async function fetchProductById({ id }: ProductRequestDTO) {
+  const { data } = await client.get<ProductDetailDTO>(`/products/${id}`);
+  return data;
+}
+
+async function productDeleteById({ id }: ProductRequestDTO) {
+  return await client.delete<Promise<void>>(`/products/${id}`);
+}
+
+async function productToggleDisableById({
+  id,
+  is_active,
+}: ProductToggleDisableRequestDTO) {
+  return await client.patch<Promise<void>>(`/products/${id}`, { is_active });
+}
+
+export {
+  fetchProducts,
+  fetchProductById,
+  productToggleDisableById,
+  productDeleteById,
+};
