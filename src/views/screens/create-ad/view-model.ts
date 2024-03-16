54f92600e71c9c880ba6615d4aef6a4606234e3e
RilsonO <rilson2.0@gmail.com>
2024-03-16 15:19:06 -0300

    refactor: Convert CreateAd screen to MVVM architecture.

---
+++ src/views/screens/create-ad/view-model.ts
@@ -0,0 +1,244 @@
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { AppNavigatorRoutesProps } from '@routes/app.routes';
+import { useToast } from 'native-base';
+import { useEffect, useState } from 'react';
+import uuid from 'react-native-uuid';
+import * as ImagePicker from 'expo-image-picker';
+import { HomeTabsNavigatorRoutesProps } from '@routes/home-tabs.routes';
+import { IProduct } from '@interfaces/product.interface';
+import { IPhoto } from '@interfaces/photo.interface';
+import { IPaymentMethods } from '@interfaces/payment-methods.interface';
+import { AppError } from '@utils/AppError.util';
+import { useAuthViewModel } from '@hooks/use-auth.hook';
+import { maskedPriceToNumber, toMaskedPrice } from '@utils/Masks.util';
+import { client } from '@infra/http/client.http';
+
+export interface CreateAdViewModel {
+  images: IPhoto[];
+  name: string;
+  description: string;
+  isNew: string;
+  price: string;
+  acceptTrade: boolean;
+  photoIsLoading: boolean;
+  headerTitle: string;
+  handleName: (value: string) => void;
+  handleDescription: (value: string) => void;
+  handleIsNew: (value: string) => void;
+  handlePrice: (value: string) => void;
+  handleAcceptTrade: (value: boolean) => void;
+  handleGoBackToMyAdsScreen: () => void;
+  handlePhotoSelect: () => Promise<void>;
+  handleRemovePhoto: (photo: IPhoto) => void;
+  findPaymentMethod: (payment_method: IPaymentMethods) => boolean;
+  handlePaymentMethods: (payment_method: IPaymentMethods) => void;
+  handleNavigateToAdPreview: () => void;
+}
+
+function useCreateAdViewModel(): CreateAdViewModel {
+  const { user } = useAuthViewModel();
+  const { navigate } = useNavigation<AppNavigatorRoutesProps>();
+  const { navigate: navigateTabs } =
+    useNavigation<HomeTabsNavigatorRoutesProps>();
+  const toast = useToast();
+  const route = useRoute();
+
+  const params = route.params as IProduct;
+
+  const [images, setImages] = useState<IPhoto[]>([]);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [isNew, setIsNew] = useState<string>('');
+  const [price, setPrice] = useState('');
+  const [acceptTrade, setAcceptTrade] = useState(false);
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [paymentMethods, setPaymentMethods] = useState<IPaymentMethods[]>([]);
+  const [isActive, setIsActive] = useState(true);
+  const [imagesToDelete, setImagesToDelete] = useState<string[]>([]);
+  const [headerTitle, setHeaderTitle] = useState('Criar anúncio');
+
+  function handleName(value: string) {
+    setName(value);
+  }
+
+  function handleDescription(value: string) {
+    setDescription(value);
+  }
+
+  function handleIsNew(value: string) {
+    setIsNew(value);
+  }
+
+  function handlePrice(value: string) {
+    setPrice(value);
+  }
+
+  function handleAcceptTrade(value: boolean) {
+    setAcceptTrade(value);
+  }
+
+  function handleGoBackToMyAdsScreen() {
+    navigateTabs('myAds');
+  }
+
+  async function handlePhotoSelect() {
+    setPhotoIsLoading(true);
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+      if (photoSelected.canceled) return;
+      if (photoSelected.assets[0].uri) {
+        const fileExtension = photoSelected.assets[0].uri.split('.').pop();
+        const photoFile = {
+          name: `${String(uuid.v4())}.${fileExtension}`.toLowerCase(),
+          uri: photoSelected.assets[0].uri,
+          type: `${photoSelected.assets[0].type}/${fileExtension}`,
+        };
+        setImages((prev) => [photoFile, ...prev]);
+      }
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível utilizar a foto. Tente novamente mais tarde.';
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  }
+
+  function handleRemovePhoto(photo: IPhoto) {
+    setImages((prev) =>
+      prev.filter((item) => {
+        if (
+          item.name === photo.name &&
+          photo.uri.match(`${client.defaults.baseURL}/images/`)
+        ) {
+          setImagesToDelete((prev) => [...prev, photo.name]);
+        }
+        return item.name !== photo.name;
+      })
+    );
+  }
+
+  function findPaymentMethod(payment_method: IPaymentMethods): boolean {
+    return paymentMethods.includes(payment_method);
+  }
+
+  function handlePaymentMethods(payment_method: IPaymentMethods) {
+    const existMethod = findPaymentMethod(payment_method);
+    if (existMethod) {
+      setPaymentMethods((prev) =>
+        prev.filter((item) => item !== payment_method)
+      );
+    } else {
+      setPaymentMethods((prev) => [...prev, payment_method]);
+    }
+  }
+
+  function handleNavigateToAdPreview() {
+    if (images.length <= 0) {
+      return toast.show({
+        title: 'Adicione ao menos uma foto do seu produto.',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
+    if (name === '') {
+      return toast.show({
+        title: 'Informe o título do seu anúncio.',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
+    if (description === '') {
+      return toast.show({
+        title: 'Informe a descrição do seu anúncio.',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
+    if (isNew === '') {
+      return toast.show({
+        title: 'Informe o estado do seu produto.',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
+    const rawPrice = Number(maskedPriceToNumber(price)) * 100;
+    if (price === '' || rawPrice <= 0) {
+      return toast.show({
+        title: 'Informe o valor do seu produto.',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
+    if (paymentMethods.length <= 0) {
+      return toast.show({
+        title: 'Informe ao menos um método de pagamento.',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
+    navigate('previewAd', {
+      user,
+      product_images: images,
+      name,
+      description,
+      is_new: isNew === 'Produto novo',
+      price: rawPrice,
+      accept_trade: acceptTrade,
+      payment_methods: paymentMethods,
+      imagesToDelete: imagesToDelete,
+      id: params?.id,
+      is_active: isActive,
+    });
+  }
+
+  useEffect(() => {
+    if (params) {
+      setImages(params.product_images);
+      setName(params.name);
+      setDescription(params.description);
+      setIsNew(params.is_new ? 'Produto novo' : 'Produto usado');
+      setPrice(toMaskedPrice(String(params.price)));
+      setAcceptTrade(params.accept_trade);
+      setPaymentMethods(params.payment_methods);
+      setIsActive(params?.is_active ?? true);
+      setImagesToDelete([]);
+      setHeaderTitle('Editar anúncio');
+    }
+  }, [params]);
+
+  return {
+    images,
+    name,
+    description,
+    isNew,
+    price,
+    acceptTrade,
+    photoIsLoading,
+    headerTitle,
+    handleName,
+    handleDescription,
+    handleIsNew,
+    handlePrice,
+    handleAcceptTrade,
+    handleGoBackToMyAdsScreen,
+    handlePhotoSelect,
+    handleRemovePhoto,
+    findPaymentMethod,
+    handlePaymentMethods,
+    handleNavigateToAdPreview,
+  };
+}
+
+export { useCreateAdViewModel };
---
+++ src/views/screens/create-ad/view.tsx
@@ -0,0 +1,263 @@
+import {
+  FlatList,
+  HStack,
+  Pressable,
+  ScrollView,
+  Skeleton,
+  Switch,
+  Text,
+  VStack,
+  useTheme,
+} from 'native-base';
+import { ArrowLeft, Plus, X } from 'phosphor-react-native';
+import { useCreateAdViewModel } from './view-model';
+import { Input } from '@views/components/input/view';
+import { maskedPriceToNumber, toMaskedPrice } from '@utils/Masks.util';
+import { Checkbox } from '@views/components/checkbox/view';
+import { Radio } from '@views/components/radio/view';
+import { Button } from '@views/components/button/view';
+import { ProductSmallPhoto } from '@views/components/product-small-photo/view';
+
+const PHOTO_SIZE = 100;
+
+export function CreateAd() {
+  const { colors, sizes } = useTheme();
+  const {
+    images,
+    name,
+    description,
+    isNew,
+    price,
+    acceptTrade,
+    photoIsLoading,
+    headerTitle,
+    handleName,
+    handleDescription,
+    handleIsNew,
+    handlePrice,
+    handleAcceptTrade,
+    handleGoBackToMyAdsScreen,
+    handlePhotoSelect,
+    handleRemovePhoto,
+    findPaymentMethod,
+    handlePaymentMethods,
+    handleNavigateToAdPreview,
+  } = useCreateAdViewModel();
+
+  return (
+    <VStack flex={1} safeAreaTop>
+      <HStack w='full' alignItems='center' justifyContent='center' px='6'>
+        <Pressable
+          position='absolute'
+          left={0}
+          ml='6'
+          onPress={handleGoBackToMyAdsScreen}
+        >
+          <ArrowLeft size={sizes[6]} color={colors.gray[700]} />
+        </Pressable>
+
+        <Text color='gray.700' fontFamily='bold' fontSize='lg+'>
+          {headerTitle}
+        </Text>
+      </HStack>
+
+      <ScrollView px='6' contentContainerStyle={{ paddingBottom: 20 }}>
+        <Text color='gray.600' fontFamily='bold' fontSize='md' mt={6} mb={1}>
+          Imagens
+        </Text>
+
+        <Text color='gray.500' fontFamily='regular' fontSize='sm'>
+          Escolha até 3 imagens para mostrar o quando o seu produto é incrível!
+        </Text>
+
+        <FlatList
+          data={images}
+          keyExtractor={(item) => item.name}
+          renderItem={({ item }) => (
+            <VStack>
+              <ProductSmallPhoto
+                source={{ uri: item.uri }}
+                alt='Foto do produto'
+                size={PHOTO_SIZE}
+              />
+              <Pressable
+                w='4'
+                h='4'
+                rounded='full'
+                bg='gray.600'
+                alignItems='center'
+                justifyContent='center'
+                position='absolute'
+                top={1}
+                right={1}
+                onPress={() => handleRemovePhoto(item)}
+              >
+                <X size={12} color={'white'} />
+              </Pressable>
+            </VStack>
+          )}
+          ListHeaderComponent={
+            photoIsLoading ? (
+              <Skeleton
+                w={PHOTO_SIZE}
+                h={PHOTO_SIZE}
+                rounded={6}
+                startColor='gray.500'
+                endColor='gray.400'
+              />
+            ) : null
+          }
+          ListFooterComponent={
+            images.length < 3 ? (
+              <Pressable
+                w={100}
+                h={100}
+                rounded={6}
+                bg='gray.300'
+                alignItems='center'
+                justifyContent='center'
+                onPress={handlePhotoSelect}
+              >
+                <Plus size={sizes[6]} color={colors.gray[400]} />
+              </Pressable>
+            ) : null
+          }
+          contentContainerStyle={{
+            paddingVertical: sizes[6],
+            gap: 8,
+          }}
+          horizontal
+        />
+
+        <Text color='gray.600' fontFamily='bold' fontSize='md' mb='3'>
+          Sobre o produto
+        </Text>
+
+        <Input
+          placeholder='Título do anúncio'
+          value={name}
+          onChangeText={handleName}
+        />
+
+        <Input
+          placeholder='Descrição do produto'
+          value={description}
+          onChangeText={handleDescription}
+          h='40'
+          multiline
+        />
+
+        <Radio
+          data={['Produto novo', 'Produto usado']}
+          name='Estado do produto'
+          accessibilityLabel='Escolha o estado do produto'
+          value={isNew}
+          onChange={(newValue) => {
+            handleIsNew(newValue);
+          }}
+        />
+
+        <Text color='gray.600' fontFamily='bold' fontSize='md' mt={6} mb={3}>
+          Venda
+        </Text>
+
+        <Input
+          leftElement={
+            <Text color='gray.700' fontFamily='regular' fontSize='md' ml='4'>
+              R$
+            </Text>
+          }
+          placeholder='Valor do produto'
+          value={price}
+          onChangeText={(text) => {
+            if (text === '0,0' || text === '0,') {
+              handlePrice('');
+              return;
+            }
+
+            const firstMaskedText = toMaskedPrice(text);
+            const firstMaskedTextConvertToNumber =
+              maskedPriceToNumber(firstMaskedText);
+            const cleanMaskedText = toMaskedPrice(
+              firstMaskedTextConvertToNumber
+            );
+            handlePrice(cleanMaskedText);
+          }}
+        />
+
+        <Text color='gray.600' fontFamily='bold' fontSize='md' mt={6} mb={3}>
+          Aceita troca?
+        </Text>
+        <Switch
+          size='sm'
+          alignSelf='flex-start'
+          offTrackColor='gray.300'
+          onTrackColor='blue.400'
+          isChecked={acceptTrade}
+          onToggle={handleAcceptTrade}
+        />
+
+        <Text fontSize='md' fontFamily='bold' color='gray.600' mt={6} mb={3}>
+          Meios de pagamento aceitos
+        </Text>
+        <Checkbox
+          value='boleto'
+          isChecked={findPaymentMethod('boleto')}
+          onChange={() => {
+            handlePaymentMethods('boleto');
+          }}
+          label={'Boleto'}
+        />
+        <Checkbox
+          value='pix'
+          isChecked={findPaymentMethod('pix')}
+          onChange={() => {
+            handlePaymentMethods('pix');
+          }}
+          label='Pix'
+        />
+        <Checkbox
+          value='cash'
+          isChecked={findPaymentMethod('cash')}
+          onChange={() => {
+            handlePaymentMethods('cash');
+          }}
+          label='Dinheiro'
+        />
+        <Checkbox
+          value='card'
+          isChecked={findPaymentMethod('card')}
+          onChange={() => {
+            handlePaymentMethods('card');
+          }}
+          label='Cartão de Crédito'
+        />
+        <Checkbox
+          value='deposit'
+          isChecked={findPaymentMethod('deposit')}
+          onChange={() => {
+            handlePaymentMethods('deposit');
+          }}
+          label='Deposito Bancário'
+        />
+      </ScrollView>
+
+      <HStack w='full' safeAreaBottom bg='white' pt='6' px='6'>
+        <Button
+          title='Cancelar'
+          flex={1}
+          bgColor='gray.300'
+          marginRight={2}
+          onPress={handleGoBackToMyAdsScreen}
+        />
+        <Button
+          title='Avançar'
+          flex={1}
+          bgColor='gray.700'
+          marginLeft={2}
+          onPress={handleNavigateToAdPreview}
+        />
+      </HStack>
+    </VStack>
+  );
+}
