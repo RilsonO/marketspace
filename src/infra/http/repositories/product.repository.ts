cae0877f6899816ebec3c55517c0d0d36d33b666
RilsonO <rilson2.0@gmail.com>
2024-03-16 15:16:51 -0300

    feat: Create new request methods.

--- src/infra/http/repositories/product.repository.ts
+++ src/infra/http/repositories/product.repository.ts
@@ -1,9 +1,15 @@
 import { client } from '../client.http';
 import {
+  ProductCreateRequestDTO,
+  ProductCreateResponseDTO,
   ProductDTO,
+  ProductDeleteImagesRequestDTO,
   ProductDetailDTO,
+  ProductInsertImagesRequestDTO,
+  ProductInsertImagesResponseDTO,
   ProductRequestDTO,
   ProductToggleDisableRequestDTO,
+  ProductUpdateRequestDTO,
   ProductsRequestDTO,
 } from '@dtos/product.dtos';
 
@@ -28,9 +34,52 @@ async function productToggleDisableById({
   return await client.patch<Promise<void>>(`/products/${id}`, { is_active });
 }
 
+async function productCreate(body: ProductCreateRequestDTO) {
+  return await client.post<ProductCreateResponseDTO>(`/products`, body);
+}
+
+async function productUpdate({ id, ...body }: ProductUpdateRequestDTO) {
+  return await client.put(`/products/${id}`, { ...body });
+}
+
+async function productInsertImages({
+  product_id,
+  product_images,
+}: ProductInsertImagesRequestDTO) {
+  const formData = new FormData();
+  formData.append('product_id', product_id);
+  product_images.map((photo) => {
+    if (photo.uri) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      formData.append('images', photo as any);
+    }
+  });
+
+  return await client.post<ProductInsertImagesResponseDTO>(
+    `/products/images`,
+    formData,
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    }
+  );
+}
+
+async function productDeleteImagesById({
+  productImagesIds,
+}: ProductDeleteImagesRequestDTO) {
+  return await client.delete(`/products/images`, {
+    data: { productImagesIds },
+  });
+}
 export {
   fetchProducts,
   fetchProductById,
   productToggleDisableById,
   productDeleteById,
+  productCreate,
+  productInsertImages,
+  productDeleteImagesById,
+  productUpdate,
 };
